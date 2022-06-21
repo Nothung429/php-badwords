15397b5c18ba63af9e865b23fd7d6bdb080db8eb
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-21 15:14:40 +0200

    add php and functioning censor

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    $text = "Hai mai lodato il Sole, Cortigiana?";
+    $forbidden = $_GET["forbidden"];
+    $censoredText = str_replace($forbidden, "***" ,$text);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PhP Censor</title>
+    </head>
+
+    <body>
+        <header>
+            <h1><?= ($text) ?></h1>
+            <h2>lunghezza:<?= strlen($text) ?></h2>
+            <h1><?= ($censoredText) ?></h1>
+            <h2>lunghezza:<?= strlen($censoredText) ?></h2>
+        </header>        
+    </body>
+</html>
